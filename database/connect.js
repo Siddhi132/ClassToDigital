9b3609f190d04ed6052a6b8a33203b28e2b64ae0
Siddhi Doshi <siddhikdoshi@gmail.com>
2023-03-04 19:30:46 +0530

    Revert "Updated Code"
    This reverts commit 510380e19db1085e66e1a3d15140ed8f4a4091a6.

--- database/connect.js
+++
@@ -1,8 +0,0 @@
-uri = "mongodb://localhost/c2dTesting";
-const mongoose = require('mongoose');
-
-const connectDB = () => {
-    return mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-}
-
-module.exports = connectDB;
